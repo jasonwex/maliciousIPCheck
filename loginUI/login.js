e97cd0f03f978ebb41ffa7b576b69201896d3961
jasonwex <jasonwex@gmail.com>
2024-07-16 13:05:45 -0500

    converted LoginUI/login.js to typescript

--- loginUI/login.js
+++ loginUI/login.js
@@ -1,27 +1,43 @@
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  const ip = document.getElementById('ip').value;
-
-  const mockLoginURL = 'http://localhost:3000/login';
-  const lessMockedLoginURL = 'http://localhost:3002/login';
-
-
-  fetch(lessMockedLoginURL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ username, password, ip })
-  })
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('message').textContent = data;
+// document.getElementById('loginForm').addEventListener('submit', function (event) {
+//   event.preventDefault();
+var _a;
+//   const username = document.getElementById('username').value;
+//   const password = document.getElementById('password').value;
+//   const ip = document.getElementById('ip').value;
+//   const mockLoginURL = 'http://localhost:3000/login';
+//   const lessMockedLoginURL = 'http://localhost:3002/login';
+(_a = document.getElementById('loginForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var ip = document.getElementById('ip').value;
+    var mockLoginURL = 'http://localhost:3000/login';
+    var lessMockedLoginURL = 'http://localhost:3002/login';
+    fetch(lessMockedLoginURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username: username, password: password, ip: ip })
     })
-    .catch(error => {
-      console.error('Error:', error);
-      document.getElementById('message').textContent = 'Failed to connect to server';
+        .then(function (response) { return response.text(); })
+        .then(function (data) {
+        var messageContent = document.getElementById('message');
+        if (messageContent) {
+            messageContent.textContent = data;
+        }
+        else {
+            console.error('Error: Response not received');
+        }
+    })
+        .catch(function (error) {
+        console.error('Error:', error);
+        var messageElement = document.getElementById('message');
+        if (messageElement) {
+            messageElement.textContent = 'Failed to connect to server';
+        }
+        else {
+            console.error('Element with ID "message" not found.');
+        }
     });
 });
---
+++ loginUI/login.ts
@@ -0,0 +1,46 @@
+// document.getElementById('loginForm').addEventListener('submit', function (event) {
+//   event.preventDefault();
+
+//   const username = document.getElementById('username').value;
+//   const password = document.getElementById('password').value;
+//   const ip = document.getElementById('ip').value;
+
+//   const mockLoginURL = 'http://localhost:3000/login';
+//   const lessMockedLoginURL = 'http://localhost:3002/login';
+document.getElementById('loginForm')?.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const username = (document.getElementById('username') as HTMLInputElement).value;
+  const password = (document.getElementById('password') as HTMLInputElement).value;
+  const ip = (document.getElementById('ip') as HTMLInputElement).value;
+
+  const mockLoginURL = 'http://localhost:3000/login';
+  const lessMockedLoginURL = 'http://localhost:3002/login';
+
+
+  fetch(lessMockedLoginURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username, password, ip })
+  })
+    .then(response => response.text())
+    .then(data => {
+      const messageContent = document.getElementById('message')
+      if (messageContent) {
+        messageContent.textContent = data;
+      } else {
+        console.error('Error: Response not received');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      const messageElement = document.getElementById('message');
+      if (messageElement) {
+        messageElement.textContent = 'Failed to connect to server';
+      } else {
+        console.error('Element with ID "message" not found.');
+      }
+    });
+});
